2da402528fc3c751f5fa5406e0a170416f4b21d7
Stefano Dolci <ste.dolci16@gmail.com>
2022-07-20 11:06:56 +0200

    Start

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// Main tempalte file.
+// @package aquila
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress theme</title>
+</head>
+<body>
+    Hello wordpress
+</body>
+</html>
