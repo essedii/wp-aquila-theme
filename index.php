91cbb60c2febc8ca7d485658ed71f4a68e8295fe
Stefano Dolci <ste.dolci16@gmail.com>
2022-07-30 10:54:01 +0200

    updated index

--- index.php
+++ index.php
@@ -7,38 +7,50 @@ get_header();
 <div id="primary">
     <main id="main" class="site-main mt-5" role='main'>
         <?php
-        if (have_posts()) :
-        ?>
-            <div class="container">
-                <?php
+        if (have_posts()) : ?>
+        <div class="container">
+            <?php
                 if (is_home() && !is_front_page()) {
                 ?>
-                    <header class="mb-5">
-                        <h1 class="page-title">
-                            <?php single_post_title(); ?>
-                        </h1>
-                    </header>
-                <?php
+            <header class="mb-5">
+                <h1 class="page-title">
+                    <?php single_post_title(); ?>
+                </h1>
+            </header>
+            <?php
                 }
                 ?>
-                <div class="row">
-                    <?php
+            <div class="row">
+                <?php
+                
+
                     while (have_posts()) : the_post();
+
+                       {
                     ?>
-                        <div class="col-lg-4 col-md-6 col-sm-12">
-                            <?php
-                            get_template_part('template-parts/content');
-                            ?>
-                        </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
                     <?php
+                        }
+
+                        get_template_part('template-parts/content');
+
+                       {
+                            ?>
+                </div>
+                <?php
+                        }
                     endwhile;
                     ?>
-                </div>
-                   
-                
             </div>
-        <?php 
+
+
+        </div>
+        <?php
+        else :
+
+            get_template_part('template-parts/content-none');
         endif;
+        get_template_part('template-parts/content-none')
         ?>
     </main>
 </div>
