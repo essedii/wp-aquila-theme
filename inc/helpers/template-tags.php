1a8aa0de87e7bb183e53176c8250026252a12969
Stefano Dolci <ste.dolci16@gmail.com>
2022-07-30 13:25:45 +0200

    Update excerpt function

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -55,18 +55,17 @@ function aquila_post_on()
     echo '<span class"posted-one text-secondary">' . $posted_on . ' </span>';
 }
 
-function aquila_the_excerpt($trim_character_count = 0)
-{
-    if (!has_excerpt() || 0 === $trim_character_count) {
-        the_excerpt();
-        return;
-    }
+function aquila_the_excerpt( $trim_character_count ) {
+    $excerpt = wp_strip_all_tags( get_the_excerpt() ); 
+   
+	if (strlen($excerpt) < $trim_character_count ) {
+		the_excerpt();
+		return;
+	}
 
-    $excerpt = wp_strip_all_tags(get_the_excerpt());
-    $excerpt = substr($excerpt, 0, $trim_character_count);
-    $excerpt = substr($excerpt, 0, stripos($excerpt, ' '));
-
-    echo $excerpt . '[...]';
+	$excerpt = substr( $excerpt, 0, $trim_character_count );
+	$excerpt = substr( $excerpt, 0, strrpos( $excerpt, ' ' ) );
+	echo $excerpt . ' [...]';
 }
 
 function aquila_posted_by()
