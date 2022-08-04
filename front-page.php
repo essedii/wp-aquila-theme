502bd3d4ec038a99f5013c93dfa0a02f328f3577
Stefano Dolci <ste.dolci16@gmail.com>
2022-08-04 14:09:10 +0200

    1

--- front-page.php
+++ front-page.php
@@ -4,38 +4,26 @@
 // include_once'header.php';
 get_header();
 ?>
-<div class=" d-flex row" style="height: 100%;" >
 <?php
+$featured_img_url_left = get_the_post_thumbnail_url(144);
+$featured_img_url_center = get_the_post_thumbnail_url(139);
+$featured_img_url_right = get_the_post_thumbnail_url(142);
+?>
+<div class=" d-flex row" style="height: 100%;">
 
-$args = array(
-    'post_type' => 'post',
-    'post_status' => 'publish',
-    'category_name' => 'home',
-    'posts_per_page' => 3,
-);
-$arr_posts = new WP_Query( $args );
- 
-if ( $arr_posts->have_posts() ) :
- 
-    while ( $arr_posts->have_posts() ) :
-        $arr_posts->the_post();
-        ?>
-        <div class="col p-0 mh-auto bg-primary align-item-center  img-hover-zoom style="  width:
-        100%;
-    height:100%; "  id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <?php
-             $featured_img_url = get_the_post_thumbnail_url(); 
-            if ( has_post_thumbnail() ) :
-
-                echo '<img src="'.$featured_img_url.'" 
+    <?php echo ' <div class="col p-0 mh-auto bg-primary align-item-center  img-hover-zoom " style="width: 100%; height:100%; ">
+                        <img 
+                            src="' . $featured_img_url_left . '" 
+                            style=" min-height:100%; max-width:100%; object-fit: cover;  object-position: 50% 50%; "> 
+                        <button class="btn " style="position: absolute;"> video</button>
+                </div>';
+    echo '
+                <div class="col p-0 mh-auto bg-primary align-item-center  img-hover-zoom " style="width: 100%; height:100%; ">
+                    <img src="' . $featured_img_url_center . '" 
                 style=" min-height:100%; max-width:100%; object-fit: cover;  object-position: 50% 50%;"
-               "
-                " >'; 
-            endif;
-            ?>
-           
-        </div>
-        <?php
-    endwhile;
-endif;
-
+                >  </div>';
+    echo '<div class="col p-0 mh-auto bg-primary align-item-center  img-hover-zoom " style="width: 100%; height:100%; "><img src="' . $featured_img_url_right . '" 
+                style=" min-height:100%; max-width:100%; object-fit: cover;  object-position: 50% 50%;"
+                > </div>'; ?>
+</div>
+</div>
