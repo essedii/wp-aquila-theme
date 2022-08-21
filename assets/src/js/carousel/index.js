b8914eda7bf7128f0ad37b4630d27a25f6173e99
Stefano Dolci <ste.dolci16@gmail.com>
2022-08-21 13:43:48 +0200

    1

--- assets/src/js/carousel/index.js
+++ assets/src/js/carousel/index.js
@@ -4,12 +4,25 @@
       this.initiateCarousel();
     }
 
+    
+
     initiateCarousel() {
-      $(".video-carousel").slick({
-        
-      });
-    }
+      $(".slider").slick({
+        nextArrow: '<button class="right" style="background: none; border: none"><i class="fa fa-chevron-right fa-2xl m-2"></i></button>',
+        prevArrow: '	<button class="left" style="background: none; border: none"><i class="fa fa-chevron-left fa-2xl m-2"></i></button>'
+        // arrows: false
+      // })
+      // $('.left').click(function(){
+      //   $('.slider').slick('slickPrev');
+      // })
+      
+      // $('.right').click(function(){
+      //   $('.slider').slick('slickNext');
+      // })
+    })
   }
+}
 
   new SlickCarousel();
 })(jQuery);
+
--- template-parts/components/posts-carousel.php
+++ template-parts/components/posts-carousel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Posts Carousel
  *
@@ -12,20 +13,27 @@ $args = [
 	'update_post_term_cache' => false,
 ];
 
-$post_query = new \WP_Query( $args );
+$post_query = new \WP_Query($args);
 ?>
-<div class="video-carousel ">
-<?php
-						if ( have_posts() ) :
-							while ( have_posts() ) : the_post();
-							?>
-							<div class="m-3">
-							<?php the_content(); 	?>
-							</div>
-							<?php				endwhile;
-						else :
-							get_template_part( 'template-parts/content-none' );
-						endif;
-						?>
+<div class="slider d-flex flex-row align-items-center justify-content-center ">
+	<?php
+	if (have_posts()) :
+		while (have_posts()) : the_post();
+	?>
+	<div class=" ">
+		<!-- <button class="left" style="background: none; border: none">
+			<i class="fa fa-chevron-left fa-2xl m-2"></i>
+		</button> -->
+		
+		<div> <?php the_content(); 	?></div>
+
+		<!-- <button class="right" style="background: none; border: none"><i class="fa fa-chevron-right fa-2xl m-2"></i></button> -->
+	</div>
+	<?php endwhile;
+	else :
+		get_template_part('template-parts/content-none');
+	endif;
+	?>
 
 </div>
+
