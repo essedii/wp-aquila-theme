3b1d41ef6278999bb3c6464426d2ab9b07a1037d
Stefano Dolci <ste.dolci16@gmail.com>
2022-08-24 20:48:31 +0200

    add z-index to nav

--- searchform.php
+++ searchform.php
@@ -8,11 +8,12 @@
  ?>
 
 
-<form role="search" method="get" class="form-inline search-form my-2 my-lg-0" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="search__form form-inline search-form  " action="<?php echo esc_url( home_url( '/' ) ); ?>">
 
-    <div class="d-flex flex-row"> 
-    <input  style="border: none;" class=" "  autocomplete="off"   placeholder="Search" value="<?php the_search_query(); ?> " aria-label="Search" name="s" >
-    <button style="border: none; background: none;" class="ms-1 my-2 my-sm-0" type="submit"><?php echo esc_attr_x( '', 'submit button', 'aquila' ); ?>   <i class="fa  fa-magnifying-glass"></i></button>
+    <div class="d-flex flex-row align-items-center
+    "> 
+    <input  style="height:20px" class="search__form"  autocomplete="off"   placeholder="Search" value="<?php the_search_query(); ?> " aria-label="Search" name="s" >
+    <button style="border: none;" class="search__form__btn ms-1 my-2 my-sm-0" type="submit"><?php echo esc_attr_x( '', 'submit button', 'aquila' ); ?>   <i class="fa  fa-magnifying-glass" style="color: gray;"></i></button>
     </div>
 
 </form>
